1599432a5daf719582110783df248a4b45eb2639
mcclayac <anthony.mcclay@me.com>
2020-08-07 01:20:26 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print('Dart Quick Start Chapter 5');
+  print('6. Anonymous Functions');
+  //https://learning.oreilly.com/library/view/quick-start-guide/9781484255629/A490744_1_En_6_Chapter.html
+
+  
+}
